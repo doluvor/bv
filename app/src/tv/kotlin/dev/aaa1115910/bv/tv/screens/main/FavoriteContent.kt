9c7624db54b3edce1a79ecc4b366ca774eec92cd
doluvor <doluvor@126.com>
2026-03-29 15:58:53 +0900

    feat: add FavoriteContent component for main screen

---
+++ app/src/tv/kotlin/dev/aaa1115910/bv/tv/screens/main/FavoriteContent.kt
@@ -0,0 +1,199 @@
+package dev.aaa1115910.bv.tv.screens.main
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.tv.material3.LocalContentColor
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Tab
+import androidx.tv.material3.TabRow
+import androidx.tv.material3.Text
+import dev.aaa1115910.biliapi.entity.FavoriteFolderMetadata
+import dev.aaa1115910.bv.R
+import dev.aaa1115910.bv.entity.proxy.ProxyArea
+import dev.aaa1115910.bv.tv.component.videocard.SmallVideoCard
+import dev.aaa1115910.bv.tv.activities.video.VideoInfoActivity
+import dev.aaa1115910.bv.util.ifElse
+import dev.aaa1115910.bv.util.fInfo
+import dev.aaa1115910.bv.viewmodel.user.FavoriteViewModel
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun FavoriteContent(
+    modifier: Modifier = Modifier,
+    navFocusRequester: FocusRequester,
+    favoriteViewModel: FavoriteViewModel = koinViewModel()
+) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val logger = KotlinLogging.logger("FavoriteContent")
+    var currentIndex by remember { mutableIntStateOf(0) }
+    val showLargeTitle by remember { derivedStateOf { currentIndex < 4 } }
+    val titleFontSize by animateFloatAsState(
+        targetValue = if (showLargeTitle) 48f else 24f,
+        label = "title font size"
+    )
+    val focusRequester = remember { FocusRequester() }
+    val defaultFocusRequester = remember { FocusRequester() }
+    var focusOnTabs by remember { mutableStateOf(true) }
+    val lazyGridState = rememberLazyGridState()
+
+    val currentTabIndex by remember {
+        derivedStateOf {
+            favoriteViewModel.favoriteFolderMetadataList.indexOf(favoriteViewModel.currentFavoriteFolderMetadata)
+        }
+    }
+
+    val updateCurrentFavoriteFolder: (folderMetadata: FavoriteFolderMetadata) -> Unit =
+        { folderMetadata ->
+            favoriteViewModel.currentFavoriteFolderMetadata = folderMetadata
+            favoriteViewModel.favorites.clear()
+            favoriteViewModel.resetPageNumber()
+            favoriteViewModel.updateFolderItems(force = true)
+        }
+
+    LaunchedEffect(Unit) {
+        scope.launch(Dispatchers.IO) {
+            favoriteViewModel.favorites.clear()
+            favoriteViewModel.resetPageNumber()
+            favoriteViewModel.updateFolderItems(force = true)
+        }
+    }
+
+    Scaffold(
+        modifier = modifier.focusRequester(navFocusRequester),
+        topBar = {
+            Box(
+                modifier = Modifier.padding(start = 48.dp, top = 24.dp, bottom = 8.dp, end = 48.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "${stringResource(R.string.user_homepage_favorite)} - ${favoriteViewModel.currentFavoriteFolderMetadata?.title ?: ""}",
+                        fontSize = titleFontSize.sp
+                    )
+                    Text(
+                        text = stringResource(
+                            R.string.load_data_count,
+                            favoriteViewModel.favorites.size
+                        ),
+                        color = Color.White.copy(alpha = 0.6f)
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        LazyVerticalGrid(
+            modifier = Modifier.padding(innerPadding),
+            state = lazyGridState,
+            columns = GridCells.Fixed(4),
+            contentPadding = PaddingValues(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            item(
+                span = { GridItemSpan(4) }
+            ) {
+                TabRow(
+                    modifier = Modifier
+                        .focusRequester(defaultFocusRequester)
+                        .onFocusChanged { focusOnTabs = it.hasFocus }
+                        .focusRequester(focusRequester),
+                    selectedTabIndex = currentTabIndex,
+                    separator = { Spacer(modifier = Modifier.width(12.dp)) },
+                ) {
+                    favoriteViewModel.favoriteFolderMetadataList.forEachIndexed { index, folderMetadata ->
+                        Tab(
+                            modifier = Modifier
+                                .ifElse(index == 0, Modifier.focusRequester(focusRequester)),
+                            selected = currentTabIndex == index,
+                            onFocus = {
+                                if (favoriteViewModel.currentFavoriteFolderMetadata != folderMetadata) {
+                                    updateCurrentFavoriteFolder(folderMetadata)
+                                }
+                            },
+                            onClick = { updateCurrentFavoriteFolder(folderMetadata) }
+                        ) {
+                            Box(
+                                modifier = Modifier.height(32.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(horizontal = 16.dp, vertical = 6.dp),
+                                    text = folderMetadata.title,
+                                    color = LocalContentColor.current,
+                                    style = MaterialTheme.typography.labelLarge
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            itemsIndexed(favoriteViewModel.favorites) { index, favorite ->
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    SmallVideoCard(
+                        data = favorite,
+                        onClick = {
+                            VideoInfoActivity.actionStart(
+                                context = context,
+                                aid = favorite.avid,
+                                proxyArea = ProxyArea.checkProxyArea(favorite.title)
+                            )
+                        },
+                        onFocus = {
+                            currentIndex = index
+                            //预加载
+                            if (index + 20 > favoriteViewModel.favorites.size) {
+                                scope.launch(Dispatchers.IO) {
+                                    logger.fInfo { "Preloading favorite data" }
+                                    favoriteViewModel.updateFolderItems()
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
